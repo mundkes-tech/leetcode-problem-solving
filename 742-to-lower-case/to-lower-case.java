62f7910dd744cdd0c5b7f7097ee9d2f75e5d93da
Mohil Mundke <mundkemohil@gmail.com>
2026-02-23 10:02:27 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (13.91%) - LeetSync

---
+++ 742-to-lower-case/to-lower-case.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String toLowerCase(String s) {
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     if(Character.isUpperCase(ch)){
+
+        //     }
+        // }
+        String lowercasestring = s.toLowerCase();
+
+        return lowercasestring;
+    }
+}
