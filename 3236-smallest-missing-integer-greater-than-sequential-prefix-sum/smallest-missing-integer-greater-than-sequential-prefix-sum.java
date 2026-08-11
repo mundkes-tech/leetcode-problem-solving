093601ba011918bb300b0beb0a16b1b1528836f6
Mohil Mundke <mundkemohil@gmail.com>
2026-08-11 14:23:51 +0530

    Time: 1 ms (93.72%) | Memory: 44 MB (64.16%) - LeetSync

--- 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
+++ 3236-smallest-missing-integer-greater-than-sequential-prefix-sum/smallest-missing-integer-greater-than-sequential-prefix-sum.java
@@ -1,22 +1,50 @@
+// class Solution {
+//     public int missingInteger(int[] nums) 
+//     {
+//         int sum=nums[0];
+//         HashSet<Integer> set = new HashSet<>();
+//         boolean flag = true;
+//         for(int j=1;j<nums.length;j++){
+//             if(nums[j] - nums[j-1] != 1){
+//                 flag = false;
+//             };
+//             if(nums[j] == nums[j-1]+1 && flag == true){
+//                 sum+=nums[j];
+//             }
+//             else if(nums[j]>=sum){
+//                 set.add(nums[j]);
+//                 System.out.println(nums[j]);
+//             };
+//         }
+//         set.add(nums[0]);
+//         while (set.contains(sum)) {
+//             sum++;
+//         }
+
+//         return sum;
+//     }
+// }
+
+
 class Solution {
     public int missingInteger(int[] nums) 
     {
         int sum=nums[0];
         HashSet<Integer> set = new HashSet<>();
-        boolean flag = true;
+
+        for(int num : nums){
+            set.add(num);
+        }
+        
         for(int j=1;j<nums.length;j++){
-            if(nums[j] - nums[j-1] != 1){
-                flag = false;
-            };
-            if(nums[j] == nums[j-1]+1 && flag == true){
+            if(nums[j] == nums[j-1]+1){
                 sum+=nums[j];
             }
-            else if(nums[j]>=sum){
-                set.add(nums[j]);
-                System.out.println(nums[j]);
-            };
+            else{
+                break;
+            }
         }
-        set.add(nums[0]);
+
         while (set.contains(sum)) {
             sum++;
         }
