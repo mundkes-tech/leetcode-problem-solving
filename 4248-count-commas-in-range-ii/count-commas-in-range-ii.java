3019035e382458a406925dcd3fef360bdad43669
Mohil Mundke <mundkemohil@gmail.com>
2026-09-09 11:47:07 +0530

    Time: 1 ms (99.36%) | Memory: 42.8 MB (24.36%) - LeetSync

---
+++ 4248-count-commas-in-range-ii/count-commas-in-range-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public long countCommas(long n) {
+        long total = 0;
+
+        long start = 1000;
+        long commas = 1;
+
+        while (start <= n) {
+            long end = Math.min(n, start * 1000 - 1);
+
+            total += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return total;
+    }
+}
